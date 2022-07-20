c14cad7ab5cd2ba0d786520e2b421115076d9aaf
levib00 <levib8@hotmail.com>
2022-07-20 00:40:04 -0400

    Game now works with buttons

--- rps.js
+++ rps.js
@@ -8,32 +8,32 @@ button.addEventListener('click', () => {
     roundCounter();
     playRound(playerEntry);
     winner()
-});
-});
     
+});
+});
+
 function playRound(playerEntry) {
     
     let playerSelection = playerEntry.toLowerCase();
     let computerSelection = computerPlay();
     playerSelection = playerPlay(playerSelection);
     playerPlay(playerSelection);
-    roundWinner(computerSelection, playerSelection)
-
-    console.log('Player score: ' + playerPoints + ' CPU score: ' + computerPoints); 
+    roundWinner(computerSelection, playerSelection) 
 }
+document.getElementById("score").innerHTML = `Player score: ${playerPoints} CPU score: ${computerPoints}`
 function playerPlay(playerSelection) {
     if (playerSelection == 'rock') {
-        console.log('Player has played rock!')
+        document.getElementById("player-choice").innerHTML = 'Player has played rock!'
         playerSelection = 1;
         return playerSelection
     }
     else if (playerSelection == 'paper') {
-        console.log('Player has played paper!')
+        document.getElementById("player-choice").innerHTML = 'Player has played paper!'
         playerSelection = 2;
         return playerSelection
     }
     else if (playerSelection == 'scissors') {
-        console.log('Player has played scissors!')
+        document.getElementById("player-choice").innerHTML = 'Player has played scissors!'
         playerSelection = 3;
         return playerSelection
     }
@@ -43,46 +43,46 @@ function computerPlay() {
     let computerPicker = Math.floor(Math.random()*3)+1;
 
     if (computerPicker == 1) {
-            console.log('Computer has played rock!')
+        document.getElementById("cpu-choice").innerHTML = 'Computer has played rock!'
             return computerPicker
     }
         else if (computerPicker == 2) {
-            console.log('Computer has played paper!')
+            document.getElementById("cpu-choice").innerHTML = 'Computer has played paper!'
             return computerPicker
     }
         else if (computerPicker == 3) {
-            console.log('Computer has played scissors!')
+            document.getElementById("cpu-choice").innerHTML = 'Computer has played scissors!'
             return computerPicker
     }
 }
 
 function roundWinner(computerSelection, playerSelection1) {
     if (computerSelection === playerSelection1) {
-        alert('It\'s a draw!');
+        document.getElementById("round-results").innerHTML ='It\'s a draw!';
     }
     else if (playerSelection1 == 1 && computerSelection == 2) {
         computerPoints++;
-        alert('Computer wins! Paper beats rock!')
+        document.getElementById("round-results").innerHTML = 'Computer wins! Paper beats rock!';
     }
     else if (playerSelection1 == 2 && computerSelection == 3) {
         computerPoints++;
-        alert('Computer wins! Scissors beats paper!')
+        document.getElementById("round-results").innerHTML = 'Computer wins! Scissors beats paper!'
     }
     else if (playerSelection1 == 3 && computerSelection == 1) {
         computerPoints++;
-        alert('Computer wins! Rock beats scissors!')
+        document.getElementById("round-results").innerHTML = 'Computer wins! Rock beats scissors!'
     } 
     else if (playerSelection1 == 3 && computerSelection == 2) {
         playerPoints++;
-        alert('Player wins! Scissors beats paper!')
+        document.getElementById("round-results").innerHTML = 'Player wins! Scissors beats paper!'
     }
     else if (playerSelection1 == 1 && computerSelection == 3) {
         playerPoints++;
-        alert('Player wins! Rock beats scissors!')
+        document.getElementById("round-results").innerHTML = 'Player wins! Rock beats scissors!'
     }
     else if (playerSelection1 == 2 && computerSelection == 1) {
         playerPoints++;
-        alert('Player wins! Paper beats rock!')
+        document.getElementById("round-results").innerHTML = 'Player wins! Paper beats rock!'
     }
     else {
         console.log('idk')
@@ -91,21 +91,22 @@ function roundWinner(computerSelection, playerSelection1) {
 }
 
 let i = 1
-function roundCounter(playerPoints, computerPoints) {
-    console.log (`Round ${i}!`)
+function roundCounter() {
+    document.getElementById("score").innerHTML = `Player score: ${playerPoints} CPU score: ${computerPoints}`
+    document.getElementById("rounds").innerHTML = `Round ${i}!`
     i++
 }
-    
+document.getElementById("rounds").innerHTML = `Round ${i}!`
+
 function winner() {
     if (playerPoints == 3 || computerPoints == 3) {
         if (playerPoints > computerPoints) {
-            console.log('You win this match! Good Job!')
+            document.getElementById("match-results").innerHTML = 'You win this match! Good Job!'
         }
         else if (playerPoints < computerPoints) {
-            console.log('you lose this match! Maybe next Time!')
+            document.getElementById("match-results").innerHTML = 'you lose this match! Maybe next Time!'
         }
         else if (playerPoints === computerPoints)
-            console.log('It\'s a draw this time!')
+        document.getElementById("match-results").innerHTML = 'It\'s a draw this time!'
     }
 }
-
